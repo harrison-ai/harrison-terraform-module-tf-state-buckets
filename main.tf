c0af640e27aad273477fd7c43e10ba7ddcdcb9ae
Carl Hattingh <carl.hattingh@harrison.ai>
2022-03-04 00:02:52 +1100

    chore: cleanup

--- main.tf
+++ main.tf
@@ -1,16 +1,5 @@
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
-  # acl    = "private"
-  # server_side_encryption_configuration {
-  #   rule {
-  #     apply_server_side_encryption_by_default {
-  #       sse_algorithm = "AES256"
-  #     }
-  #   }
-  # }
-  # versioning {
-  #   enabled = true
-  # }
   tags = {
     Name = var.bucket_name
   }
